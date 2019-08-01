83df12883c24f6a227862631d6d030162cf9912d
Tu Manh Vu <glmanhtu@gmail.com>
2019-08-01 14:37:43 +0100

    Fixed the incorrect of getting script file's location

--- ddi-pride-datasync.sh
+++ ddi-pride-datasync.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd .. && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd ${SCRIPT_DIR}
+echo `pwd`
 source definitions.sh
 
 
