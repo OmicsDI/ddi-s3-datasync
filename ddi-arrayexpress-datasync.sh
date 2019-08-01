078a7fc6c8c22d9f3fd6da580dae9e8666b50fac
Tu Manh Vu <glmanhtu@gmail.com>
2019-08-01 14:51:10 +0100

    Fixed command args incorrected

--- ddi-arrayexpress-datasync.sh
+++ ddi-arrayexpress-datasync.sh
@@ -9,11 +9,11 @@ JOB_EMAIL="gdass@ebi.ac.uk"
 JOB_NAME="ddi-arrayexpress-datasync"
 
 bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q ${QUEUE} -u ${JOB_EMAIL} -J ${JOB_NAME}-experiments \
-    ./command.sh logs/${JOB_NAME}-experiments.$(date +"%m-%d-%Y").log \
+    ./command.sh -o "logs/${JOB_NAME}-experiments.$(date +"%m-%d-%Y").log" \
     "./venv/bin/python arrayexpress-splitter/experiments.py ${AE_EXPERIMENTS_FILE} ${AE_EXPERIMENTS_OUT_DIR}" \
     "./sync.sh ${AE_EXPERIMENTS_OUT_DIR} s3://${S3_BUCKET}/data/arrayexpress/experiments"
 
 bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q ${QUEUE} -u ${JOB_EMAIL} -J ${JOB_NAME}-protocols \
-    ./command.sh logs/${JOB_NAME}-protocols.$(date +"%m-%d-%Y").log \
+    ./command.sh -o "logs/${JOB_NAME}-protocols.$(date +"%m-%d-%Y").log" \
     "./venv/bin/python arrayexpress-splitter/experiments.py ${ARRAYEXPRESS_PROTOCOLS_FILE} ${AE_PROTOCOLS_OUT_DIR}" \
     "./sync.sh ${AE_PROTOCOLS_OUT_DIR} s3://${S3_BUCKET}/data/arrayexpress/protocols"
